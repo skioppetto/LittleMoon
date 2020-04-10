222a072c8cd99458d287eb1c984fcb933390f1e2
Alberto Coppini <skioppetto@gmail.com>
2020-04-11 01:18:16 +0200

    added blinking while connecting

--- lab/sketch_wifimanager/sketch_wifimanager.ino
+++ lab/sketch_wifimanager/sketch_wifimanager.ino
@@ -1,7 +1,8 @@
 // Author: Alberto Coppini
 // date: 2020-04-11
 // test libreria wifimanager. Viene testata la callback che verrà utilizzata per scrivere correttamente il testo sul led,
-// inoltre viene testata la funzione di reset tramite pressione lunga del tasto
+// inoltre viene testata la funzione di reset tramite pressione lunga del tasto. 
+// Il led lampeggia fino a che riesce a collegarsi con ultime impostazioni, in tal caso si spegne oppure passa in modalità AP, in tal caso rimane acceso.
 
 #include <FS.h>                   //this needs to be first, or it all crashes and burns...
 
@@ -12,21 +13,30 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 
+#include <Ticker.h>
+
 #define PIN_RST_BUTTON 5
 
+WiFiManager wifiManager;
+Ticker tick;
+
+
+void blinker(){
+  int state = digitalRead(LED_BUILTIN);
+  digitalWrite(LED_BUILTIN, !state);
+  }
 
 //gets called when WiFiManager enters configuration mode
 void configModeCallback (WiFiManager *myWiFiManager) {
+  tick.detach();
   digitalWrite(LED_BUILTIN, LOW);
   pinMode(PIN_RST_BUTTON, INPUT_PULLUP);
 }
 
- WiFiManager wifiManager;
-
 void setup() {
- 
-  pinMode(LED_BUILTIN, OUTPUT);
   
+  pinMode(LED_BUILTIN, OUTPUT);
+  tick.attach(0.1, blinker);
    //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
  
@@ -46,7 +56,7 @@ void setup() {
     ESP.reset();
     delay(5000);
   }
-
+  tick.detach();
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
